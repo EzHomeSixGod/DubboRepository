1ba42d2250e13f661a92127036acc84e67d37e88
EzHomeSixGod <953625227@qq.com>
2018-11-30 17:47:42 +0800

    add redis registry

---
+++ dubbo-demo-core/src/main/java/com/ezhomesixgod/App.java
@@ -0,0 +1,13 @@
+package com.ezhomesixgod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
