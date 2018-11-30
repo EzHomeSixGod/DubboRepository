b9f2fe046b86b9aeb3fcbc56abcad1bd68e264ad
EzHomeSixGod <953625227@qq.com>
2018-11-30 13:24:06 +0800

    【add】mod file

--- dubbo-demo-provider/src/main/java/com/ezhomesixgod/config/EncryptPropertyPlaceholderConfigurer.java
+++ dubbo-demo-provider/src/main/java/com/ezhomesixgod/config/EncryptPropertyPlaceholderConfigurer.java
@@ -3,11 +3,7 @@ package com.ezhomesixgod.config;
 import com.ezhomesixgod.core.util.DESUtils;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
-/**
- * @author renyang
- * @date 2018 10 22
- * @version 1.0
- */
+
 public class EncryptPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
 	
 	private String[] encryptPropNames = { "userName", "password", "url", "driver"};
