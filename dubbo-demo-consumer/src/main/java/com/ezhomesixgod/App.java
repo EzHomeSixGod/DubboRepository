a70142902e0114f72a6d5eaae31b7c7da3451af9
EzHomeSixGod <953625227@qq.com>
2018-12-03 14:47:21 +0800

    mod file

---
+++ dubbo-demo-consumer/src/main/java/com/ezhomesixgod/App.java
@@ -0,0 +1,25 @@
+package com.ezhomesixgod;
+
+import com.ezhomesixgod.service.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-mybatis.xml");
+        context.start();
+        // Obtaining a remote service proxy
+        DemoService demoService = (DemoService)context.getBean("demoService");
+        // Executing remote methods
+        String hello = demoService.sayHello("world");
+        // Display the call result
+        System.out.println(hello);
+        System.out.println( "Hello World!" );
+    }
+}
